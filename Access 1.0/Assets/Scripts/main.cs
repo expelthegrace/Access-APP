168456fb8d1ceab1ad1370dda68160c96ebce95b
Marc <marc.palomar.soler@gmail.com>
2019-12-09 13:50:43 +0100

    check stand implemented
    function that checks if the stand column exists, if so it checkes it to 1
    if not, it creates the column and then checkes to 1 the field

--- Access 1.0/Assets/Scripts/main.cs
+++ Access 1.0/Assets/Scripts/main.cs
@@ -13,16 +13,29 @@ public class main : MonoBehaviour
     [SerializeField]
     public RawImage camImage;
 
+    //Buttons
+    public Button scanButton;
+    public Button stopScanButton;
+
+    //Credentials data
     public string eventname = "accesstest";
     public string servername = "quy910.yourstats.es";
     public string username = "quy910";
     public string password = "C0sm02019";
     public string dbname = "quy910";
+
     public string tableConvidats = "accesstest";
     public string tableStands = "accesstestStands";
+    public string standName = "ingeniastand3";
 
     //Global
+    public string QRlecture = "111";
+
     public string queryResult = "--";
+
+    private bool queryEnded = false;
+    private bool runningQuery = false;
+    private string lastQueryName = "";
 
     void Start()
     {
@@ -33,26 +46,64 @@ public class main : MonoBehaviour
         debugText.text = "";
     }
 
+    public void Update()
+    {
+        // Since DDBB calls are coroutines the followeing system manages when these coroutines are finished
+        //When a DDBB corotutine is called its called alongisde a name, this name is checked here to trigger the proper response to the finished coroutine
+        if (queryEnded && !runningQuery)
+        {
+            Debug.Log("rountine finished: " + lastQueryName);
+            switch (lastQueryName)
+            {
+                //Called after checking if the stand column exists
+                case "checkColumnExists":
+                    Debug.Log("columnsheck result: " + queryResult);
+                    if (queryResult == "0|\n") AddColumn();
+                    else checkColumnStand();
+                    break;
+                //Called if needed to add stand column
+                case "addColumnStand":
+                    checkColumnStand();
+                    break;
+            }
+
+            queryEnded = false;
+        }
+    }
+
+    void AddColumn()
+    {
+        Debug.Log("1111 ");
+        string sql = "ALTER TABLE " + dbname + "." + tableConvidats + " ADD COLUMN " + standName + " VARCHAR(1) NOT NULL DEFAULT 0";
+        StartCoroutine(ExecuteQuery(sql, "addColumnStand"));
+    }
+
+    void checkColumnStand()
+    {
+        Debug.Log("222 ");
+        string sql = "UPDATE " + dbname + "." + tableConvidats + " SET " + standName + " = 1 WHERE CODIGO_BARRAS = " + QRlecture;
+        StartCoroutine(ExecuteQuery(sql, "checkColumnStand"));
+    }
+
+    public void checkAssistant(string barcodevalue)
+    {
+        string sql = "SELECT COUNT(*) AS TOTAL FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + tableConvidats + "' AND COLUMN_NAME = '" + standName + "'";
+        StartCoroutine(ExecuteQuery(sql, "checkColumnExists", "TOTAL"));
+
+        //The secuence continues in Update()
+    }
+
     IEnumerator StartAuthorization()
     {
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
     }
 
-    public void InserNewMarc()
-    {
-        //string query = "UPDATE " + eventname + " SET APELLIDO1 = '" + System.DateTime.Now.ToString() + "' where CODIGO_BARRAS = '05/12/2019 18:34:17' ;";
-        string query = "SELECT * FROM " + tableConvidats;
-       
-        StartCoroutine(ExecuteQuery(query, "ID|NOMBRE|APELLIDO1"));
 
-        //It has to go to the update function
-        //debugText.text = queryResult;
-    }
-
-    IEnumerator ExecuteQuery(string query, string returnFields = "CODIGO_BARRAS")
+    IEnumerator ExecuteQuery(string query, string queryName, string returnFields = "CODIGO_BARRAS")
     {
         if (query != "")
         {
+            runningQuery = true;
             WWWForm form = new WWWForm();
             form.AddField("servername",servername);
             form.AddField("username",username);
@@ -68,47 +119,48 @@ public class main : MonoBehaviour
             }
 
             var data = www.downloadHandler.text;
-            
-            queryResult = data.ToString();
 
+            runningQuery = false;
+            queryEnded = true;
+            lastQueryName = queryName;
+
+            queryResult = data.ToString();
+            debugText.text = queryResult;
         }
     }
 
-    IEnumerator GetData()
+    IEnumerator ExecuteMultiQuery(string query, string returnFields = "CODIGO_BARRAS")
     {
-        //gameObject.guiText.text = "Loading...";
-
-        Debug.Log("Crida");
-
-        WWWForm form = new WWWForm();
-        form.AddField("CODIGO_BARRAS", System.DateTime.Now.ToString());
-
-        UnityWebRequest www = UnityWebRequest.Post("https://yourstats.es/Unity_App/scripts/query.php", form);
-        while (!www.isDone)
+        if (query != "")
         {
-            yield return www.SendWebRequest();
+            WWWForm form = new WWWForm();
+            form.AddField("servername",servername);
+            form.AddField("username",username);
+            form.AddField("password",password);
+            form.AddField("dbname", dbname);
+            form.AddField("query", query);
+            form.AddField("returnFields", returnFields);
+
+            UnityWebRequest www = UnityWebRequest.Post("https://yourstats.es/Unity_App/scripts/multiquery.php", form);
+            while (!www.isDone)
+            {
+                yield return www.SendWebRequest();
+            }
+
+
+            var data = www.downloadHandler.text;
+            
+            queryResult = data.ToString();
+            debugText.text = queryResult;
         }
-
-        var data = www.downloadHandler.text;
-
-        // Debug.Log(data.ToString());
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-            debugText.text = www.error;
-        }
-        else
-        {
-            Debug.Log("Form upload complete!");
-            debugText.text = "Form upload complete!";
-        }
-
-
     }
 
     public void Scan()
     {
+        stopScanButton.enabled = true;
+        scanButton.enabled = false;
+
+        camImage.enabled = true;
         if (camImage)
         {
             QRCodeManager.CameraSettings camSettings = new QRCodeManager.CameraSettings();
@@ -126,9 +178,15 @@ public class main : MonoBehaviour
         }
     }
 
+
+
     public void StopScanning()
     {
         QRCodeManager.Instance.StopScanning();
+        camImage.enabled = false;
+
+        stopScanButton.enabled = false;
+        scanButton.enabled = true;
     }
 
     void OnEnable()
